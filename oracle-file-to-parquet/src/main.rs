c1065d33212bf89422ea3a06580f814d8f963cd5
Kent Williams <k3nt00@gmail.com>
2023-03-10 14:08:53 -0800

    local oracle file to parquet test working

--- oracle-file-to-parquet/src/main.rs
+++ oracle-file-to-parquet/src/main.rs
@@ -1,15 +1,21 @@
 use anyhow::anyhow;
 use chrono::{TimeZone, Utc};
-use file_store::{FileStore, FileType, FileInfo, FileInfoStream, Settings};
+use file_store::{FileInfo, FileInfoStream, FileStore, FileType};
 use futures::StreamExt;
-use helium_crypto::PublicKey;
 use helium_proto::{
-    services::{poc_lora::GatewayRewardShare, poc_mobile::RadioRewardShare},
+    services::{poc_lora::GatewayRewardShare},
     Message,
 };
-use lambda_runtime::{service_fn, Error, LambdaEvent};
+use lambda_runtime::{Error};
 use serde_json::{json, Value};
-use std::{env, str::FromStr};
+use std::{str::FromStr};
+
+use parquet::{
+    data_type::{ByteArray, ByteArrayType, Int64Type},
+    file::{properties::WriterProperties, writer::SerializedFileWriter},
+    schema::parser::parse_message_type,
+};
+use std::{fs, path::Path, sync::Arc};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -22,69 +28,55 @@ fn infos(names: &'static [&str]) -> FileInfoStream {
 }
 
 async fn handler() -> Result<Value, Error> {
+    let path = Path::new("gateway_reward_share_1671643842138.parquet");
+
+    let message_type = "
+    message schema {
+        REQUIRED BYTE_ARRAY hotspot_key (UTF8);
+        REQUIRED INT64 beacon_amount;
+        REQUIRED INT64 witness_amount;
+        REQUIRED INT64 start_period;
+        REQUIRED INT64 end_period;
+    }
+    ";
+
+    let schema = Arc::new(parse_message_type(message_type).unwrap());
+    let props = Arc::new(WriterProperties::builder().build());
+    let file = fs::File::create(&path).unwrap();
+    let mut writer = SerializedFileWriter::new(file, schema, props).unwrap();
+    let mut row_group_writer = writer.next_row_group().unwrap();
+
     let file_store = FileStore::new(None, "us-west-2", "mainnet-pociot-verified")
-            .await
-            .expect("file store");
+        .await
+        .expect("file store");
 
     let key = "gateway_reward_share.1671643842138.gz";
-
     let prefix = key.split('.').next().unwrap_or("");
     let file_type = FileType::from_str(prefix)?;
-    // let store = FileStore::from_settings(settings).await?;
-
     let mut file_stream = file_store.source(infos(&["gateway_reward_share.1671643842138.gz"]));
 
-    // let mut file_stream = store.get(key).await?;
-
-    // println!("bucket is {}", bucket);
     println!("key is {}", key);
-    // println!("region is {}", region);
+
+    let mut hotspot_key_vec: Vec<ByteArray> = Vec::new();
+    let mut beacon_amount_vec: Vec<i64> = Vec::new();
+    let mut witness_amount_vec: Vec<i64> = Vec::new();
+    let mut start_period_vec: Vec<i64> = Vec::new();
+    let mut end_period_vec: Vec<i64> = Vec::new();
 
     let mut count = 0;
     while let Some(result) = file_stream.next().await {
         let msg = result?;
         count += 1;
         match file_type {
-            FileType::RadioRewardShare => {
-                let reward = RadioRewardShare::decode(msg)?;
-                let end_epoch = Utc.timestamp_opt(reward.end_epoch as i64, 0);
-                if let chrono::LocalResult::Single(end_epoch) = end_epoch {
-                    // sqlx::query(
-                    //     r#"
-                    //     INSERT INTO mobile_poc_rewards (amount, epoch_end, hotspot_key, cbsd_id)
-                    //     VALUES ($1, $2, $3, $4)
-                    //     ON CONFLICT
-                    //     DO NOTHING
-                    //     "#,
-                    // )
-                    // .bind(reward.amount as i64)
-                    // .bind(end_epoch)
-                    // .bind(PublicKey::try_from(reward.hotspot_key)?)
-                    // .bind(reward.cbsd_id)
-                    // .execute(&pool)
-                    // .await?;
-                } else {
-                    return Err(anyhow!("Unexpected end_epoch: {end_epoch:?}").into());
-                }
-            }
             FileType::GatewayRewardShare => {
                 let reward = GatewayRewardShare::decode(msg)?;
                 let end_period = Utc.timestamp_opt(reward.end_period as i64, 0);
-                if let chrono::LocalResult::Single(end_period) = end_period {
-                    // sqlx::query(
-                    //     r#"
-                    //     INSERT INTO iot_poc_rewards (beacon_amount, witness_amount, epoch_end, hotspot_key)
-                    //     VALUES ($1, $2, $3, $4)
-                    //     ON CONFLICT
-                    //     DO NOTHING
-                    //     "#
-                    //     , )
-                    //     .bind(reward.beacon_amount as i64)
-                    //     .bind(reward.witness_amount as i64)
-                    //     .bind(end_period)
-                    //     .bind(PublicKey::try_from(reward.hotspot_key)?)
-                    //     .execute(&pool).await?;
-                    println!("hotspot_key {}", reward.beacon_amount)
+                if let chrono::LocalResult::Single(_end_period) = end_period {
+                    hotspot_key_vec.push(ByteArray::from(reward.hotspot_key.clone()));
+                    beacon_amount_vec.push(reward.beacon_amount.clone() as i64);
+                    witness_amount_vec.push(reward.witness_amount.clone() as i64);
+                    start_period_vec.push(reward.start_period.clone() as i64);
+                    end_period_vec.push(reward.end_period.clone() as i64);
                 } else {
                     return Err(anyhow!("Unexpected end_epoch: {end_period:?}").into());
                 }
@@ -92,6 +84,49 @@ async fn handler() -> Result<Value, Error> {
             _ => (),
         }
     }
+
+    let mut col_number = 0;
+    while let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
+        col_number = col_number + 1;
+
+        match col_number {
+            1 => {
+                col_writer
+                    .typed::<ByteArrayType>()
+                    .write_batch(hotspot_key_vec.as_slice(), None, None)
+                    .unwrap();
+            }
+            2 => {
+                col_writer
+                    .typed::<Int64Type>()
+                    .write_batch(beacon_amount_vec.as_slice(), None, None)
+                    .unwrap();
+            }
+            3 => {
+                col_writer
+                    .typed::<Int64Type>()
+                    .write_batch(witness_amount_vec.as_slice(), None, None)
+                    .unwrap();
+            }
+            4 => {
+                col_writer
+                    .typed::<Int64Type>()
+                    .write_batch(start_period_vec.as_slice(), None, None)
+                    .unwrap();
+            }
+            5 => {
+                col_writer
+                    .typed::<Int64Type>()
+                    .write_batch(end_period_vec.as_slice(), None, None)
+                    .unwrap();
+            }
+            _ => println!("no column match"),
+        }
+        col_writer.close().unwrap();
+    }
+
+    row_group_writer.close().unwrap();
+    writer.close().unwrap();
 
     let message = format!("{count} rows of {prefix} processed.");
     Ok(json!({ "message": message }))
